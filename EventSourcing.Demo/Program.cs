82c5324cd6d8cf7e6954faba788e85a3b47166a5
Nicklas Jensen <nije@universal-robots.com>
2020-05-04 21:06:48 +0200

    Refactoring

--- EventSourcing.Demo/Program.cs
+++ EventSourcing.Demo/Program.cs
@@ -12,21 +12,19 @@ namespace EventSourcing.Demo
     {
         public static async Task Main(string[] args)
         {
-            // using var connection = EventStoreConnection.Create("ConnectTo=tcp://admin:changeit@localhost:1113");
-
-            // await connection.ConnectAsync();
-            // await TestCase(connection);
-            await TestCase(null);
-        }
-
-        private static async Task TestCase(IEventStoreConnection connection)
-        {
+            using var connection = EventStoreConnection.Create("ConnectTo=tcp://admin:changeit@localhost:1113");
+            
+            await connection.ConnectAsync();
+            
             var encoder = new JsonEncoder();
             var decoder = new JsonDecoder();
-            
-            // var store = new Framework.EventStore(connection, encoder, decoder);
-            var store = new InMemoryEventStore(encoder, decoder);
+            var store = new Framework.EventStore(connection, encoder, decoder);
 
+            await TestCase(store);
+        }
+
+        private static async Task TestCase(IEventStore store)
+        {
             var id = Guid.NewGuid();
 
             await CreateCaseAsync(store, id);
